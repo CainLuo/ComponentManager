d5024adac6a0d2f298e454c0dfe78e0ef6cf7410
CainLuo <350116542@qq.com>
2020-11-30 20:19:42 +0800

    Update ModuleAConnector.swift

--- OAComponentManager/ModuleA/ModuleAConnector.swift
+++ OAComponentManager/ModuleA/ModuleAConnector.swift
@@ -8,8 +8,11 @@
 import UIKit
 
 class ModuleAConnector: NSObject {
-    
-    static let instance = ModuleAConnector()
+        
+    static func instance() -> ModuleAConnector {
+        let connector = ModuleAConnector()
+        return connector
+    }
     
     func register() {
         ComponentManager.register(self)
@@ -23,11 +26,55 @@ extension ModuleAConnector: ComponentManagerPrt {
     }
     
     func connectToOpenURL(_ url: URL, parameters: Dictionary<String, Any>) -> UIViewController? {
+        guard canOpenURL(url) else { return nil }
+        
         let vc = ModuleAViewController.configureWith()
         return vc
     }
     
-    func connectToHandle(_ prt: Protocol) -> Any? {
+    func connectToHandle<T>(_ prt: T.Type) -> Any? {
+        if prt == ModuleAServiceProtocol.self {
+            return ModuleAConnector.instance()
+        }
+        
         return nil
+    }
+}
+
+extension ModuleAConnector: ModuleAServiceProtocol {
+    func show(_ message: String, cancel: @escaping (([String : Any]) -> Void), done: @escaping (([String : Any]) -> Void)) {
+        let cancel = UIAlertAction(title: "取消", style: .cancel) { action in
+            cancel(["alertAction":action])
+        }
+        
+        let done = UIAlertAction(title: "确定", style: .default) { action in
+            done(["alertAction":action])
+        }
+        
+        let alert = UIAlertController(title: "ModuleA", message: message, preferredStyle: .alert)
+        alert.addAction(cancel)
+        alert.addAction(done)
+        
+        if let window = UIApplication.shared.windows.first,
+           let root = window.rootViewController {
+            root.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func getItem(_ name: String, age: Int) -> ModuleAItemServiceProtocol {
+        let item = ModuleAItem()
+        item.itemName = "AAAAAAAAA"
+        item.itemAge = 1000
+        return item
+    }
+    
+    func deliveAprotocol(_ item: ModuleAItemServiceProtocol) {
+        let cancel = UIAlertAction(title: "取消", style: .cancel) { _ in }
+        let done = UIAlertAction(title: "取消", style: .default) { _ in }
+        let message = item.description()
+        let alert = UIAlertController(title: "ModuleA", message: message, preferredStyle: .alert)
+        alert.addAction(cancel)
+        alert.addAction(done)
+        UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
     }
 }
