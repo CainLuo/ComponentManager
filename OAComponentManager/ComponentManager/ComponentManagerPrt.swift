da5ea7c5c9273696d7f004bf110f847ae2c774a7
CainLuo <350116542@qq.com>
2020-11-30 20:52:55 +0800

    Update ComponentManagerPrt.swift

--- OAComponentManager/ComponentManager/ComponentManagerPrt.swift
+++ OAComponentManager/ComponentManager/ComponentManagerPrt.swift
@@ -9,6 +9,5 @@ import UIKit
 
 protocol ComponentManagerPrt: class {
     func canOpenURL(_ url: URL) -> Bool
-    func connectToOpenURL(_ url: URL, parameters: Dictionary<String, Any>) -> UIViewController?
-    func connectToHandle<T: Any>(_ prt: T.Type) -> Any?
+    func connectToOpenURL(_ url: URL, parameters: Dictionary<String, Any>?, completion: (([String: Any]) -> Void)?) -> UIViewController?
 }
